cb44f3aabc3b587d71ae03d01abe07c8a4b25b29
arvid102 <47725237+arvid102@users.noreply.github.com>
2024-06-27 19:06:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ const products = [
     { id: 4, name: "Just Drift It T-Shirt", price: 18.99, image: "images/just_drift_it.png", category: "t-shirt" },
     { id: 5, name: "Solo Leveling T-Shirt", price: 25.99, image: "images/solo_lvl_0.png", category: "t-shirt" },
     { id: 6, name: "Himiko Toga T-Shirt", price: 5.99, image: "images/himiko_toga.png", category: "t-shirt" },
+    { id: 7, name: "GTR Godzila T-Shirt", price: 5.99, image: "images/0.png", category: "t-shirt" },
+    { id: 8, name: "Nezuko Lightbox", price: 5.99, image: "images/himiko_toga.png", category: "decor" },
+    { id: 9, name: "Himiko Toga T-Shirt", price: 5.99, image: "images/himiko_toga.png", category: "t-shirt" },
+    { id: 10, name: "Toga T-Shirt", price: 5.99, image: "images/himiko_toga.png", category: "t-shirt" },
+    { id: 11, name: "imiko Toga T-Shirt", price: 5.99, image: "images/himiko_toga.png", category: "t-shirt" },
     // Add more products...
 ];
 
