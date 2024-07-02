8efc2ecfe8e225973d85ac517daf644377607add
arvid102 <47725237+arvid102@users.noreply.github.com>
2024-07-02 14:30:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ const products = [
         image: "images/cyberbunk_rebecca.png", 
         category: "t-shirt",
         description: "Show your love for Cyberpunk with this stylish Rebecca t-shirt.",
-        additionalImages: ["images/cyberpunk_rebecca2.png", "images/black_back.png"]
+        additionalImages: ["images/cyberbunk_rebecca_2.png", "images/cyberbunk_rebecca_3.png"],
+        colors: ["Red", "Blue", "Black"],
+        sizes: ["S", "M", "L", "XL"]
     },
     { id: 2, name: "I Love BMW T-Shirt", price: 15.99, image: "images/love_bmw1.png", category: "t-shirt" },
     { id: 3, name: "Gojo T-Shirt", price: 12.99, image: "images/gojo_satoru_nike.png", category: "t-shirt" },
